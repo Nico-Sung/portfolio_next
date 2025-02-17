3fe3ef4f467f37a7a15c22bc68aec6da93142706
Nico-Sng <nicolassung01@gmail.com>
2025-02-17 21:19:19 +0100

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,6 @@ import Cursor from "@/components/Cursor";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 import AboutMe from "@/components/AboutMe";
-import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
